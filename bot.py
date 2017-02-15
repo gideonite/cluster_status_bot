d423c28784f7591693ad55330e4a857468172818
Gideon Dresdner <gideond@gmail.com>
2017-02-15 19:21:28 +0100

    and there you have it
    it ain't pretty but it works

---
+++ bot.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python 
+
+import os
+import time
+import requests
+from slackclient import SlackClient
+from bs4 import BeautifulSoup as bs
+
+# starterbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+CLUSTER_STATUS_URL = "https://scicomp.ethz.ch/wiki/System_status"
+
+def parse_status_from_href(href):
+    if 'red' in href:
+        return 'down'
+    elif 'orange' in href:
+        return 'partially operational'
+    elif 'green' in href:
+        'up and running'
+    else:
+        raise Exception("unhandled system status")
+
+def fetch_cluster_status():
+    page = requests.get(CLUSTER_STATUS_URL)
+    soup = bs(page.content, "lxml")
+    tables = soup.findAll('table')
+    assert len(tables) == 2, \
+        "Invariant that there are only two tables is no longer true"
+
+    main, key = tables
+
+    rows = main.findAll('tr')
+
+    assert len(rows) == 2, \
+        "Main table no longer has 2 rows. Fail."
+
+    header, row = rows
+
+    service_names = [h.contents[0] for h in header.findAll('a')]
+
+    imgs = [a['href'] for a in row.findAll('a')]
+
+    msgs = [parse_status_from_href(img) for img in imgs]
+
+    return list(zip(service_names, msgs)), ('down' in msgs)
+
+def report(msgs, slack_client):
+    print("LOG: reporting...")
+
+    text = '```' + "\n".join([service + ": " + status for service, status in msgs]) + '```'
+
+    slack_client.api_call("chat.postMessage", \
+                          channel="@gideon", \
+                          text=text, as_user=True)
+
+REPORTED = False
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+READ_WEBSOCKET_DELAY = 1 # seconds
+if __name__ == "__main__":
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+
+        while True:
+            msgs, should_i_report = fetch_cluster_status()
+
+            if should_i_report and not REPORTED:
+                report(msgs, slack_client)
+                REPORTED = True
+            elif not should_i_report:
+                print("LOG: status all good!")
+                REPORTED = False
+
+            time.sleep(READ_WEBSOCKET_DELAY)
